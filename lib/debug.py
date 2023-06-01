007ea3ec1c81163d8d24370520c40760c590d231
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-06-01 13:45:11 -0700

    Complete lab

--- lib/debug.py
+++ lib/debug.py
@@ -3,4 +3,6 @@
 from email_address_parser import EmailAddressParser
 
 if __name__ == '__main__':
+    t1 = EmailAddressParser("talk@talk.com, john.jones@flatironschool.com, alexa@amazon.com")
+    t2 = EmailAddressParser("talk@talk.com, talk@talk.com alexa@amazon.com what john.jones@flatironschool.com, who alexa@amazon.com, where when why")
     import ipdb; ipdb.set_trace()
--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
-# your code goes here!
+import re 
+
+class EmailAddressParser:
+
+    def __init__(self, address_string):
+        self.address_string = address_string
+
+    def parse(self):
+        pattern = r"[^0-9^\W][\w.]+[@][\w.]+"
+        regex = re.compile(pattern)
+        matches = regex.findall(self.address_string)
+        unique_matches = set(matches)
+        return sorted(list(unique_matches))
